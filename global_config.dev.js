69aa77b9ea72be244da27e15edb0b79a4e1c2bb5
lhn <lhn@158834353@qq.com>
2019-09-26 15:09:16 +0800

    修改 _config.js

--- global_config.dev.js
+++ global_config.dev.js
@@ -3,6 +3,4 @@ module.exports = {
     BASE: '/home',
     // 后端地址
     API_ADDRESS: 'http://..',
-    GRAHP_ADDRESS: 'http://..',
-    XHB_ADDRESS: ''
 }
--- global_config.js
+++ global_config.js
@@ -3,7 +3,5 @@ module.exports = {
     BASE: '/home',
     // 后端地址
     API_ADDRESS: 'http://..',
-    GRAHP_ADDRESS: 'http://..',
-    XHB_ADDRESS: ''
     
 }
